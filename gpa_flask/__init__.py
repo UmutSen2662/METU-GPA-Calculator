3f060736ef7066b818c4dc049ecb1244a8e160c2
Umut Şen <umutsen2662@gmail.com>
2024-04-16 23:06:20 +0300

    pool recycle

--- gpa_flask/__init__.py
+++ gpa_flask/__init__.py
@@ -17,7 +17,7 @@ SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostnam
 )
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.sqlite"
 #app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 250
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
