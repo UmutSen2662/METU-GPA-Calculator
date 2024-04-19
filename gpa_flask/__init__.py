ec782179762efcc864d15ee41922d22d2cb208ad
Umut Şen <umutsen2662@gmail.com>
2024-04-19 18:50:11 +0300

    hid important data into .env and .cfg files

--- gpa_flask/__init__.py
+++ gpa_flask/__init__.py
@@ -1,40 +1,27 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from dotenv import dotenv_values
 from flask_bcrypt import Bcrypt
 from flask_mail import Mail
 from flask import Flask
-from os import popen
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
+
 app.jinja_env.lstrip_blocks = True
 app.jinja_env.trim_blocks = True
-app.secret_key = "6e3fa0b943bdd99b572f14ffb792f935679a10a83010ce36784a47a5632c7b2b"
+app.secret_key = dotenv_values(".env")["secret_key"]
 
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="MetuGpaCalculato",
-    password="dbpassword",
-    hostname="MetuGpaCalculator.mysql.pythonanywhere-services.com",
-    databasename="MetuGpaCalculato$default",
-)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+cfg = dotenv_values(".cfg")
+app.config.update(cfg)
 app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
     "pool_pre_ping": True,
     "pool_recycle": 280,
 }
-if popen('hostname').read() == "TUF\n":
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.sqlite"
 
+mail = Mail(app)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 unauthorized_handler = login_manager.unauthorized_handler
-
-app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_PORT"] = 587
-app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = "metugpaclaculator@gmail.com"
-app.config["MAIL_PASSWORD"] = "jhsx vnwv ptgv eitt"
-mail = Mail(app)
 
 from gpa_flask import routes
